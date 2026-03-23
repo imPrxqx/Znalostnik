34a6b5deee736ccb98bff8cb1f872b385ff0a091
imPrxqx <honza.ruzba@seznam.cz>
2026-03-23 21:53:51 +0100

    chore: delete old unused code

--- Znalostnik/frontend/src/features/rooms/components/join-room-form/join-room-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-join-room-form',
-  imports: [],
-  templateUrl: './join-room-form.html',
-  styleUrl: './join-room-form.scss',
-})
-export class JoinRoomForm {}
