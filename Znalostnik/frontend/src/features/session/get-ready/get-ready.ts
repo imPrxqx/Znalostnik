e4d4d48efa65e9841f11982d9edcb1a714d85477
imPrxqx <honza.ruzba@seznam.cz>
2026-06-23 19:00:48 +0200

    delete: code was moved

--- Znalostnik/frontend/src/features/session/get-ready/get-ready.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-get-ready',
-  imports: [],
-  templateUrl: './get-ready.html',
-  styleUrl: './get-ready.scss',
-})
-export class GetReady {}
