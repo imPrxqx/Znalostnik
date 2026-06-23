ed0843a713bb522ae8e7ebf3fa333e657186b556
imPrxqx <honza.ruzba@seznam.cz>
2026-06-23 18:40:06 +0200

    chore: update login page

--- Znalostnik/frontend/src/features/authentication/login/login.ts
+++ Znalostnik/frontend/src/features/authentication/login/login.ts
@@ -1,45 +1,23 @@
-import { RouterOutlet, RouterModule } from '@angular/router';
 import { Component, inject } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {
-  ValidatorFn,
-  AbstractControl,
-  ValidationErrors,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Authentication } from '@core/services/authentication';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatSelectModule,
     MatInputModule,
-    RouterModule,
-    MatGridListModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule,
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule,
-    CommonModule,
+    RouterLink,
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
@@ -48,7 +26,7 @@ export class Login {
   auth = inject(Authentication);
 
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
 
