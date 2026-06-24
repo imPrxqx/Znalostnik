a77d07d5de7f626a9e38dcf9eef40db3efcbade5
imPrxqx <honza.ruzba@seznam.cz>
2026-06-24 14:08:11 +0200

    feat: add guess evaluator

---
+++ Znalostnik/backend/backend/Services/Evaluators/IAnswerEvaluator.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+using backend.Models;
+
+namespace backend.Services
+{
+    public interface IAnswerEvaluator
+    {
+        string ExerciseActivityType { get; }
+        void Evaluate(RuntimeActivity activity, RuntimeAnswer answer);
+    }
+}
