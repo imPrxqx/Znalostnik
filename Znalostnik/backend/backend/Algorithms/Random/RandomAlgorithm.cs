98d567a0cc44a64b5bb9fd79deb380feacea751c
imPrxqx <honza.ruzba@seznam.cz>
2026-06-24 17:58:27 +0200

    feat: add random selection algorithm

---
+++ Znalostnik/backend/backend/Algorithms/Random/RandomAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿using backend.Models;
+
+namespace backend.Algorithms
+{
+    public class RandomAlgorithm : ISelectionAlgorithm
+    {
+        public string AlgorithmType => "random";
+
+        private readonly Random _random = new Random();
+
+        public RuntimeActivity SelectNextQuestion(
+            IReadOnlyList<RuntimeActivity> candidates,
+            AlgorithmsState state
+        )
+        {
+            RuntimeActivity activity = candidates[_random.Next(candidates.Count)];
+            return activity;
+        }
+
+        public void UpdatePlayerState(
+            AlgorithmsState state,
+            RuntimeActivity activity,
+            int correctPercentile
+        )
+        {
+            return;
+        }
+    }
+}
