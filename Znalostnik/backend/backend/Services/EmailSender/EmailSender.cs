a1358ecb68c5832362fb6771f2e3a048ed5b183c
imPrxqx <honza.ruzba@seznam.cz>
2026-06-25 16:34:11 +0200

    feat: add email sender service

---
+++ Znalostnik/backend/backend/Services/EmailSender/EmailSender.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace backend.Services.EmailSender
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailFrom = Environment.GetEnvironmentVariable("EMAIL")!;
+            var emailPassword = Environment.GetEnvironmentVariable("EMAIL_PASSWORD")!;
+
+            using var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(emailFrom, emailPassword),
+                EnableSsl = true,
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress(emailFrom),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true,
+            };
+
+            mail.To.Add(email);
+
+            await client.SendMailAsync(mail);
+        }
+    }
+}
