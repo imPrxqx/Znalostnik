62137e9969b275a0d4757700f30d2134db96c5cf
imPrxqx <honza.ruzba@seznam.cz>
2026-06-26 13:56:01 +0200

    chore: update visual of join component

--- Znalostnik/frontend/src/features/session/join/join.ts
+++ Znalostnik/frontend/src/features/session/join/join.ts
@@ -1,26 +1,11 @@
-import { RouterOutlet, RouterModule } from '@angular/router';
 import { Component, inject } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {
-  ValidatorFn,
-  AbstractControl,
-  ValidationErrors,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SessionState } from '../services/session-state';
 
 @Component({
@@ -28,17 +13,10 @@ import { SessionState } from '../services/session-state';
   imports: [
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatSelectModule,
     MatInputModule,
-    RouterModule,
-    MatGridListModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule,
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule,
     CommonModule,
   ],
   templateUrl: './join.html',
