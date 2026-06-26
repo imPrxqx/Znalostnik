2ece39b15c0f1d896662fe39a7691a07773226ad
imPrxqx <honza.ruzba@seznam.cz>
2026-06-26 13:33:06 +0200

    chore: update participant page

--- Znalostnik/frontend/src/features/session/participant/participant.ts
+++ Znalostnik/frontend/src/features/session/participant/participant.ts
@@ -1,38 +1,18 @@
 import { Component, computed, inject, linkedSignal, signal } from '@angular/core';
-import { ExerciseTask } from '@features/exercise-editor/components/exercise-task/exercise-task';
 import { SessionState } from '../services/session-state';
-import { Task } from '@shared/models/format';
-import { CommonModule } from '@angular/common';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HotPotatoParticipant } from '../modes/hot-potato/hot-potato-participant/hot-potato-participant';
+import { ClassicParticipant } from '../modes/classic/classic-participant/classic-participant';
+import { SelfStudyParticipant } from '../modes/self-study/self-study-participant/self-study-participant';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-import { FormsModule } from '@angular/forms';
-import { MatMenuModule } from '@angular/material/menu';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Hub } from '../services/hub';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { Timer } from '../timer/timer';
 @Component({
   selector: 'app-participant',
   imports: [
-    ExerciseTask,
-    MatIconModule,
-    CommonModule,
-    FormsModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatButtonModule,
-    MatProgressBarModule,
-    MatSlideToggleModule,
-    MatMenuModule,
-    MatToolbarModule,
-    Timer,
     MatProgressSpinnerModule,
+    ClassicParticipant,
+    HotPotatoParticipant,
+    SelfStudyParticipant,
   ],
   templateUrl: './participant.html',
   styleUrl: './participant.scss',
@@ -42,10 +22,6 @@ export class Participant {
   route = inject(ActivatedRoute);
   router = inject(Router);
   session = computed(() => this.state.session());
-  task = computed(() => this.state.task());
-  answer = linkedSignal(() => this.state.answer());
-  hub = inject(Hub);
-  end = signal<Date>(new Date(new Date().getTime() + 30000));
   loading = computed(() => this.state.loading());
 
   ngOnInit() {
@@ -57,15 +33,5 @@ export class Participant {
     }
 
     this.state.loadSession(sessionId);
-  }
-
-  confirmAnswer() {
-    console.log('answer confirmed: ', this.answer());
-    this.state.confirmAnswer(this.session()?.id!);
-  }
-
-  submitAnswer() {
-    console.log('answer submitted: ', this.answer());
-    this.state.submitAnswer(this.session()?.id!, this.answer()!);
   }
 }
