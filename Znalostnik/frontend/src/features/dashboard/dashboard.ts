cebd7bb13c9ba245eebcfc0e7b896eaed758b1bc
imPrxqx <honza.ruzba@seznam.cz>
2026-06-26 20:32:59 +0200

    chore: update visual of dashboard page

--- Znalostnik/frontend/src/features/dashboard/dashboard.ts
+++ Znalostnik/frontend/src/features/dashboard/dashboard.ts
@@ -1,9 +1,6 @@
-import { Component, inject } from '@angular/core';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenuModule } from '@angular/material/menu';
+import { Component, inject, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -11,27 +8,50 @@ import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Authentication } from '@core/services/authentication';
 import { RouterModule } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ExercisesManager } from './services/exercises-manager';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   imports: [
     RouterOutlet,
+    RouterLink,
     RouterModule,
-    MatGridListModule,
     MatSidenavModule,
     MatListModule,
-    MatMenuModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule,
     MatToolbarModule,
     CommonModule,
   ],
 })
 export class Dashboard {
   auth = inject(Authentication);
+  manager = inject(ExercisesManager);
+  breakPoint = inject(BreakpointObserver);
+  isMobile = signal(false);
+  isOpened = signal(true);
+
   logout() {
     this.auth.logout();
+  }
+
+  constructor() {
+    this.breakPoint.observe(['(max-width: 1200px)']).subscribe((r) => {
+      this.isMobile.set(r.matches);
+      this.isOpened.set(!r.matches);
+    });
+  }
+
+  close() {
+    if (this.isMobile()) {
+      this.isOpened.set(false);
+    }
+  }
+
+  toggle() {
+    this.isOpened.set(!this.isOpened());
   }
 }
