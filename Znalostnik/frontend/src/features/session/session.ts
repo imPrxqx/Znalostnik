fcc3527701742e5145a6595a66626bffdecf4f4e
imPrxqx <honza.ruzba@seznam.cz>
2026-06-26 13:57:41 +0200

    delete: remove unused code

--- Znalostnik/frontend/src/features/session/session.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-session',
-  imports: [],
-  templateUrl: './session.html',
-  styleUrl: './session.scss',
-})
-export class Session {}
--- Znalostnik/frontend/src/shared/models/exercise-task-document-schemas.model.ts
+++
@@ -1,6 +0,0 @@
-export const ExerciseTaskDocumentSchemas: any[] = [
-  {
-    key: 'quiz',
-    alias: $localize`:@@document.quiz:Kvíz`,
-  },
-];
