f558b5b575291d9cf2b3cd4463ee35ef42a3a0ac
imPrxqx <honza.ruzba@seznam.cz>
2026-07-05 21:16:49 +0200

    chore: update quiz command ui

--- Znalostnik/frontend/src/shared/commands/components/update-multi-choice-command-ui/update-multi-choice-command-ui.ts
+++ Znalostnik/frontend/src/shared/commands/components/update-multi-choice-command-ui/update-multi-choice-command-ui.ts
@@ -1,51 +1,48 @@
 import { Component, input, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { CommandManager } from '@features/exercise-editor/services/command-manager';
 import { AddChoiceCommand } from '@shared/commands/add-choice-command';
-import { RemoveChoiceCommand } from '@shared/commands/remove-choice-command copy';
-import { UpdateChoiceCommand } from '@shared/commands/update-choice-command';
+import { RemoveChoiceCommand } from '@shared/commands/remove-choice-command';
 import { UpdateChoiceSolutionCommand } from '@shared/commands/update-choice-solution-command';
-import { MultiChoiceOption, QuizTask } from '@shared/models/format';
+import { FieldContext } from '@shared/interfaces/field-context';
+import { MultiChoiceOption } from '@shared/models/format';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { QuizSolution } from '@shared/models/quiz';
+import { EditChoice } from '@shared/media/edit-choice/edit-choice';
 
 @Component({
   selector: 'app-update-multi-choice-command-ui',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatCheckboxModule, EditChoice],
   templateUrl: './update-multi-choice-command-ui.html',
   styleUrl: './update-multi-choice-command-ui.scss',
 })
 export class UpdateMultiChoiceCommandUi {
-  task = input.required<QuizTask>();
+  field = input.required<
+    FieldContext<{
+      choices: MultiChoiceOption;
+      solution: QuizSolution;
+    }>
+  >();
+
   commandManager = inject(CommandManager);
 
-  onCheckboxChange(optionId: string) {
-    const command = new UpdateChoiceSolutionCommand(this.task(), optionId);
-    this.commandManager.execute(command);
-  }
-
-  onInputChange(index: number, value: string) {
-    const command = new UpdateChoiceCommand(this.task().options().options[index], value);
-    this.commandManager.execute(command);
-  }
-
   isChecked(optionId: string): boolean {
-    return this.task().solution().includes(optionId);
+    return this.field().value.solution.correct.includes(optionId);
   }
 
-  static supports(item: any): boolean {
-    if (!('options' in item)) {
-      return false;
-    }
-
-    const value = item.options?.();
-    return value instanceof MultiChoiceOption;
+  onCheckboxChange(optionId: string) {
+    const command = new UpdateChoiceSolutionCommand(this.field().value.solution, optionId);
+    this.commandManager.execute(command);
   }
 
   addOption() {
-    const command = new AddChoiceCommand(this.task().options());
+    const command = new AddChoiceCommand(this.field().value.choices);
     this.commandManager.execute(command);
   }
 
   removeLastOption() {
-    const command = new RemoveChoiceCommand(this.task().options());
+    const command = new RemoveChoiceCommand(this.field().value.choices);
     this.commandManager.execute(command);
   }
 }
